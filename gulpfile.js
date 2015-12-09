547b080168a7cda48bd6bf433ffe0635db43ae3a
Badaboom1995 <kosplay@mail.ru>
2015-12-09 19:41:48 +0300

    end

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	less = require('gulp-less');
 
-gulp.task("meow", function() {
-	console.log('meow');
+gulp.task("style", function() {
+	return gulp.src('less/style.less')
+	.pipe(less([
+		paths: [path.join(__dirname,)]
+		]))
 });
--- js/script.js
+++ js/script.js
@@ -1,18 +1,104 @@
-$(document).ready(function(){ 
+
 
 	$(document).ready(function() {
  
   $("#owl-example").owlCarousel({
   	items : 1,
-  	navigation : true,
-    navigationText : ["prev","next"],
+  	navigation : false,
     pagination : false,
-    slideSpeed : 1000,
+    slideSpeed : 400,
     itemsDesktop : [1199,1],
     itemsDesktopSmall : [980,1],
     itemsTablet: [768,1],
     itemsMobile : [479,1],
+    lazyLoad : true,
   });
+  var owl = $(".owl-carousel").data('owlCarousel');
+  $(".promo-slider__tab-first").on("click", function(e){
+      e.preventDefault();
+      owl.goTo("0");
+      $(this).addClass("tab-active");
+      $(this).siblings().removeClass("tab-active");
+  })
+   $(".promo-slider__tab-second").on("click", function(e){
+      e.preventDefault();
+      owl.goTo("1");
+      $(this).addClass("tab-active");
+      $(this).siblings().removeClass("tab-active");
+  })
+
+    $("#gallery").owlCarousel({
+    items : 6,
+    navigation : false,
+    pagination : false,
+  });
+
+     $("#big-slide").owlCarousel({
+    items : 1,
+    navigation : false,
+    pagination : false,
+    autoHeight : true,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [980,1],
+    itemsTablet: [768,1],
+    itemsMobile : [479,1],
+    lazyLoad : true,
+  });
+
+
+    if($(window).width()>1199){
+           $("#reviews").owlCarousel({
+        items : 2,
+        navigation : false,
+        pagination : false,
+        
+      });
+    }
+    if($(window).width()<1200){
+      $("#reviews").removeClass("owl-carousel");
+    }
+     $(".discount__slide").css("position", "relative");
+     $(".discount__navigation").hide();
+     $("#discount").owlCarousel({
+    items : 1,
+    navigation : false,
+    pagination : true,
+    slideSpeed : 400,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [980,1],
+    itemsTablet: [768,1],
+    itemsMobile : [479,1],
+    lazyLoad : true,
+  });
+     $("#single-room__slider").owlCarousel({
+    items : 1,
+    navigation : false,
+    pagination : false,
+    slideSpeed : 400,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [980,1],
+    itemsTablet: [768,1],
+    itemsMobile : [479,1],
+    lazyLoad : true,
+  });
+
+     if($(window).width()<1200&&$(window).width()>767){
+         
+             $("#single-room__bottom-slider").owlCarousel({
+        items : 2,
+        navigation : false,
+        pagination : true,
+        slideSpeed : 400,
+        itemsDesktop : [1199,2],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,2],
+        itemsMobile : [479,2],
+        lazyLoad : true,
+      });
  
-});
+    }
+
+    
+     
+     
 });
