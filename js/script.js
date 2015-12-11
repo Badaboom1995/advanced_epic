b0195d735d8cc6899907d4cb48a200e949326674
Badaboom1995 <kosplay@mail.ru>
2015-12-11 23:15:59 +0300

    fixed points 1,2,3,4,5,8

--- js/script.js
+++ js/script.js
@@ -98,13 +98,11 @@
  
     }
 
-   var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+    $(".burger").on("click", function(){
+      $(".main-nav").toggleClass("active");
+      $(".burger").toggleClass("active");
+    })
+   
      
      
 });
